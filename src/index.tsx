7ca1df83ecfc801d1f099d81878ea9687b6f3329
Zrna <lukazrnic5@gmail.com>
2020-05-01 11:11:56 +0200

    Apollo config added

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,33 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-boost';
+
+import token from '../githubToken';
 
 import './index.scss';
 
 import App from './App';
 
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${token}`,
+  },
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
