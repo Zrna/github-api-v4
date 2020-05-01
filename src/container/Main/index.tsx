70f590b2a97d2b37ba8a14d466090423755f69bb
Zrna <lukazrnic5@gmail.com>
2020-05-01 17:43:37 +0200

    Added styles for initial view and for search container

--- src/container/Main/index.tsx
+++ src/container/Main/index.tsx
@@ -1,25 +1,43 @@
 import * as React from 'react';
 import { useState, useRef } from 'react';
 
+import './styles.scss';
+
+import Logo from '../../assets/logo.svg';
 import Profile from '../Profile';
 
 const Main = () => {
   const [ user, setUser ] = useState('');
+  const [ initialView, setInitialView ] = useState(true);
   const inputElement = useRef<HTMLInputElement>(null);
 
   const handleFormSubmit = (e: any) => {
     e.preventDefault();
+    setInitialView(false);
     if (inputElement.current) setUser(inputElement.current.value);
   };
 
+  const resetDataToDefault = () => {
+    setInitialView(true);
+    setUser('');
+  };
+  
   return (
-    <>
-      <form onSubmit={handleFormSubmit}>
-        <input type='text' ref={inputElement} placeholder='Type Github username' />
-        <button type='submit'>submit</button>
-      </form>
-      <Profile user={user} />
-    </>
+    <div className={`${initialView ? 'initial-centered' : 'container'}`}>
+      <div className='search-container'>
+        <div className='title'>
+          <img src={Logo} alt='Github logo' onClick={resetDataToDefault} />
+          <span>Github Search</span>
+        </div>
+        <form onSubmit={handleFormSubmit}>
+          <input type='text' ref={inputElement} placeholder='Type Github username' />
+          <button type='submit'>Search</button>
+        </form>
+      </div>
+      <div className='user-profile'>
+        <Profile user={user} />
+      </div>
+    </div>
   );
 };
 
