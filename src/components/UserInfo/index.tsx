3ef5797b8d2a35490362a2626847b55bba49de7e
Zrna <lukazrnic5@gmail.com>
2020-05-02 16:15:23 +0200

    Added styles for UserInfo component

--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -4,16 +4,32 @@ import { IUser } from '../../../domain/interfaces';
 
 import './styles.scss';
 
+import userIcon from '../../assets/user.svg';
+import linkIcon from '../../assets/link.svg';
+import emailIcon from '../../assets/email.svg';
+
 const UserInfo = ({ data: { user } }: { data: { user: IUser } }) => {
   const { avatarUrl, login, email, url } = user;
 
   return (
     <div className='user-info'>
-      <img src={avatarUrl} alt={login} />
-      <p>Usename: {login}</p>
-      <p>Email: {email}</p>
-      <a href={url} target='_blank' rel='noopener noreferrer'>Link to Github profile</a>
-      <hr />
+      <img className='avatar' src={avatarUrl} alt={login} />
+      <div>
+        <img className='icon' src={userIcon} alt='User icon' />
+        <span>{login}</span>
+      </div>
+      { email ?
+        <div>
+          <img className='icon' src={emailIcon} alt='Email icon' />
+          <a href={`mailto:${email}`}>{email}</a>
+        </div>
+        :
+        null
+      }
+      <div>
+        <img className='icon' src={linkIcon} alt='Link icon' />
+        <a href={url} target='_blank' rel='noopener noreferrer'>Github profile</a>
+      </div>
     </div>
   );
 };
